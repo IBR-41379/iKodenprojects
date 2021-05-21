a3412dac4f4710e33dda23bf907d56c03820ce5a
IBR-41379 <sayandeep369@gmail.com>
2021-05-21 11:53:37 +0530

    Made it more functional
    Added the op integer

--- lqdm12project.py
+++ lqdm12project.py
@@ -1,17 +1,22 @@
-Advanced Calculator lqdm12 basic.py
-num1 = input("Enter a number:")
-num2 = input("Enter a number:")
-result = float(num1) + float(num2)
-print(result)
+#Advanced Calculator lqdm12 basic.py
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter a number:"))
+#result = num1 + num2
+#print(result)
+print("Choose options")
+print("1)Addition\n2)Subtraction\n3)Division\n4)Multiplication\n5)Exponent")
+op=int(input("Enter Choice:)
 
-if op == "+":
+if op == 1:
   print(num1 + num2)
-elif op == "_":
+elif op == 2:
   print(num1 - num2)
-elif op == "/":
+elif op == 3:
   print(num1 / num2)
-elif op == "*":
+elif op == 4:
   print(num1 * num2)
+elif op == 5
+  print(num1 ** num2)
 else:
   print("Invalid operator")
 
